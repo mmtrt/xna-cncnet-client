44e6f8d627cfcf8118f958f78881a71512cd4306
Taqi Raza <mttalpur@gmail.com>
2026-08-21 00:22:56 +0500

    Increase team pairs to A-H

--- ClientCore/ProgramConstants.cs
+++ ClientCore/ProgramConstants.cs
@@ -108,7 +108,7 @@ namespace ClientCore
             return "";
         }
 
-        public static readonly List<string> TEAMS = new List<string> { "A", "B", "C", "D" };
+        public static readonly List<string> TEAMS = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H" };
 
         // Static fields might be initialized before the translation file is loaded. Change to readonly properties here.
         public static List<string> AI_PLAYER_NAMES => new List<string> { "Easy AI".L10N("Client:Main:EasyAIName"), "Medium AI".L10N("Client:Main:MediumAIName"), "Hard AI".L10N("Client:Main:HardAIName") };
